fc3f94ae8218e65f10c8c577e920a0083ca941a9
Steven Rojas <sroja@engagepeople.com>
2017-07-03 17:14:27 -0500

    Finishing pagination.

--- app/components/app.js
+++ app/components/app.js
@@ -1,6 +1,6 @@
 var root = "http://api-movies.com/api/v1/";
 
-var mymovies = angular.module('MyMovies', ['ngRoute', 'ui.router', 'ngResource'])
+var mymovies = angular.module('MyMovies', ['ngRoute', 'ui.router', 'ngResource', "bw.paging"])
 .constant("ROOT", root)
 .constant("APP_NAME", "My Movie");
 
